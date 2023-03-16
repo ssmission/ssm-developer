00dcf4e4e00c80d4e6cc0176ef70e3574f520365
Michael Becker <51341849+21beckem@users.noreply.github.com>
2023-03-16 18:02:27 +0100

    Update br.php

--- br.php
+++ br.php
@@ -8,6 +8,10 @@ if (isset($_GET['addr'])) {
 $raw_html = file_get_contents($addr);
 $html = str_ireplace("<head>", '<head><base href="'.$addr.'">', $raw_html);
 
-echo($html);
+$actual_link = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+$pattern = '/(<a[^>]*href=")([^"]*)("[^>]*>)/mi';
+$replacement = '$1'.$actual_link.'?addr=$2$3';
+
+echo preg_replace($pattern, $replacement, $html);
 
 ?>
