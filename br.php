b3594c344567b732c734496bb481f4ff573e9c04
Michael Becker <51341849+21beckem@users.noreply.github.com>
2023-03-16 15:16:59 +0100

    Update br.php

--- br.php
+++ br.php
@@ -1,6 +1,9 @@
 <?php
 
-$addr = $_GET['addr'] || 'https://google.com';
+$addr = 'https://google.com';
+if (isset($_GET['addr'])) {
+  $addr = $_GET['addr'];
+}
 
 $raw_html = file_get_contents($addr);
 $html = str_ireplace("<head>", '<head><base href="'.$addr.'">', $raw_html);
